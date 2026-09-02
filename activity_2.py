f962be3fb27c287d7d9a7d6b6b919b553a1ada39
KingSid29 <siddhantmittal103@gmail.com>
2026-09-02 15:25:13 +0100

    lesson 2

---
+++ activity_2.py
@@ -0,0 +1,12 @@
+y = "SID"
+print(y)
+x = 67
+print(x)
+s = "Codingal"
+print(s)
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+age = input("Enter your age: ")
+print("You are", age, "years old.")
